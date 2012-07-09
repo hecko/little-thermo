67d7b2f6fde120e045fd2814462c8e666c478702
Michal Belica <devel@beli.sk>
2012-07-09 15:51:35 +0200

    * coding style cleanup

--- src/temp.c
+++ src/temp.c
@@ -22,157 +22,162 @@
 #include <netdb.h>
 #include <sys/un.h>
 
-#define handle_error(msg) \
-    do { perror(msg); exit(EXIT_FAILURE); } while (0)
 #define SEND_INTERVAL 5000
+#define LOCAL_SERVER_PORT 15000
 
 unsigned char version;
 littleWire *myLittleWire = NULL;
 char *conf_user;
 
-void signal_handler(int sig) {
-	switch(sig) {
+void signal_handler(int sig)
+{
+	switch (sig) {
 
-		case SIGTERM:
-			syslog(LOG_NOTICE,"received a SIGTERM signal");
-			exit(0);
-			break;
-		case SIGINT:
-			syslog(LOG_NOTICE, "received a SIGINT signal");
-			exit(0);
-			break;
+	case SIGTERM:
+		syslog(LOG_NOTICE, "received a SIGTERM signal");
+		exit(0);
+		break;
+	case SIGINT:
+		syslog(LOG_NOTICE, "received a SIGINT signal");
+		exit(0);
+		break;
 	}
 }
 
-void InitTemp(void) {
+void InitTemp(void)
+{
+	myLittleWire = littleWire_connect();
 
-        myLittleWire = littleWire_connect();
+	if (myLittleWire == NULL) {
+		syslog(LOG_ERR,
+			   "Little Wire could not be found! Make sure you are running this as root!\n");
+		exit(EXIT_FAILURE);
+	}
 
-        if(myLittleWire == NULL){
-                syslog(LOG_ERR, "Little Wire could not be found! Make sure you are running this as root!\n");
-                exit(EXIT_FAILURE);
-        }
+	version = readFirmwareVersion(myLittleWire);
+	syslog(LOG_ERR, "Little Wire firmware version: %d.%d\n",
+		   ((version & 0xF0) >> 4), (version & 0x0F));
 
-        version = readFirmwareVersion(myLittleWire);
-        syslog(LOG_ERR, "Little Wire firmware version: %d.%d\n",((version & 0xF0)>>4),(version&0x0F));
-
-        pinMode(myLittleWire,PIN2,INPUT);
+	pinMode(myLittleWire, PIN2, INPUT);
 }
 
-float ReadTemp(void) {
-    unsigned int adcValue;
-	adcValue=analogRead(myLittleWire, ADC_TEMP_SENS);
-	return (float)((0.888*adcValue)-235.8);
+float ReadTemp(void)
+{
+	unsigned int adcValue;
+	adcValue = analogRead(myLittleWire, ADC_TEMP_SENS);
+	return (float)((0.888 * adcValue) - 235.8);
 }
 
 //sender part which regularly sends data to the cloud 
-void* Sender(void *arg) {
+void *Sender(void *arg)
+{
+	CURL *curl_handle;
+	CURLcode res;
+	curl_handle = curl_easy_init();
+	char *curl_data;
 
-        CURL *curl_handle;
-        CURLcode res;
-        curl_handle = curl_easy_init();
-        char *curl_data;
-
-        while(1){
+	while (1) {
 		float temp_c;
 		temp_c = ReadTemp();
-                if (curl_handle) {
-                        //prepare post data
-                        asprintf(&curl_data, "user=%s&key=temp&val=%f", conf_user, temp_c);
-                        //set URL and POST data
-                        curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, curl_data);
-                        curl_easy_setopt(curl_handle, CURLOPT_URL, "http://linode.blava.net/meter/");
-                        //set no progress meter
-                        curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
-                        res = curl_easy_perform(curl_handle);
-                        if(res != CURLE_OK) {
-                                syslog(LOG_NOTICE, "curl_easy_perform() failed: %s\n",curl_easy_strerror(res));
-                        }
-                }
-                syslog(LOG_INFO, "%s\n", curl_data);
-                //free(curl_data);
+		if (curl_handle) {
+			//prepare post data
+			asprintf(&curl_data, "user=%s&key=temp&val=%f", conf_user, temp_c);
+			//set URL and POST data
+			curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, curl_data);
+			curl_easy_setopt(curl_handle, CURLOPT_URL,
+							 "http://linode.blava.net/meter/");
+			//set no progress meter
+			curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+			res = curl_easy_perform(curl_handle);
+			if (res != CURLE_OK) {
+				syslog(LOG_NOTICE, "curl_easy_perform() failed: %s\n",
+					   curl_easy_strerror(res));
+			}
+		}
+		syslog(LOG_INFO, "%s\n", curl_data);
+		//free(curl_data);
 
-                delay(SEND_INTERVAL);
-        }
+		delay(SEND_INTERVAL);
+	}
 	curl_easy_cleanup(curl_handle);
 }
 
-void *servlet(void *arg)                    /* servlet thread */
-{	FILE *fp = (FILE*)arg;            /* get & convert the data */
+void *servlet(void *arg)
+{								/* servlet thread */
+	FILE *fp = (FILE *) arg;	/* get & convert the data */
 	float temp_c;
 
 	temp_c = ReadTemp();
-	fprintf(fp, "%f", temp_c);                             /* echo it back */
-	fclose(fp);                   /* close the client's channel */
-	return 0;                           /* terminate the thread */
+	fprintf(fp, "%f", temp_c);	/* echo it back */
+	fclose(fp);					/* close the client's channel */
+	return 0;					/* terminate the thread */
 }
 
-void* Server(void *arg) {
-	#define LOCAL_SERVER_PORT 15000
+void *Server(void *arg)
+{
+	int sd, port;
+	struct sockaddr_in addr;
+	int optval;
 
 	syslog(LOG_NOTICE, "Starting server code...");
 
-	int sd, port;
-	struct sockaddr_in addr;
-    int optval;
-
 	memset(&addr, 0, sizeof(struct sockaddr_in));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(LOCAL_SERVER_PORT);
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(LOCAL_SERVER_PORT);
 
-	sd=socket(AF_INET, SOCK_STREAM, 0);
-	if(sd<0) {
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sd < 0) {
 		syslog(LOG_NOTICE, "cannot open socket \n");
 		exit(1);
 	}
 	optval = 1;
-    setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+	setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
 
-	if ( bind(sd, (struct sockaddr*)&addr, sizeof(addr) ) != 0 )
+	if (bind(sd, (struct sockaddr *)&addr, sizeof(addr)) != 0)
 		syslog(LOG_NOTICE, "problem with server bind");
 
-	if ( listen(sd, 10) != 0 ) {
+	if (listen(sd, 10) != 0) {
 		perror("listen");
-		syslog(LOG_NOTICE, "problem with server listener");	
+		syslog(LOG_NOTICE, "problem with server listener");
 		exit(1);
 	} else {
-        int as;
+		int as;
 		pthread_t child;
 		FILE *fp;
 
-		while (1)                         /* process all incoming clients */
-		{
-			     /* accept connection */
-	        if ( (as = accept(sd, 0, 0)) == -1 ) {
-        		perror("accept");
-        		syslog(LOG_NOTICE, "problem with socket accept");	
-        		exit(1);
-            }
-			fp = fdopen(as, "r+");           /* convert into FILE* */
-			pthread_create(&child, 0, servlet, fp);       /* start thread */
-			pthread_detach(child);                      /* don't track it */
+		while (1) {				/* process all incoming clients */
+			/* accept connection */
+			if ((as = accept(sd, 0, 0)) == -1) {
+				perror("accept");
+				syslog(LOG_NOTICE, "problem with socket accept");
+				exit(1);
+			}
+			fp = fdopen(as, "r+");	/* convert into FILE* */
+			pthread_create(&child, 0, servlet, fp);	/* start thread */
+			pthread_detach(child);	/* don't track it */
 		}
 	}
-
 	syslog(LOG_NOTICE, "Server code ending...");
 }
 
 int main(int argc, char **argv)
 {
-	openlog(argv[0],LOG_NOWAIT|LOG_PID,LOG_USER);
+	openlog(argv[0], LOG_NOWAIT | LOG_PID, LOG_USER);
 	//disable stdout buffering
 	setbuf(stdout, NULL);
-	syslog(LOG_NOTICE, "----------Starting temperatire monitoring server----------");
+	syslog(LOG_NOTICE,
+		   "----------Starting temperatire monitoring server----------");
 
 	if (argc != 2) {
-		syslog(LOG_ERR,"Please run the program with username: %s <username>\n",argv[0]);
-		return -1; 
+		syslog(LOG_ERR, "Please run the program with username: %s <username>\n",
+			   argv[0]);
+		return -1;
 	}
-    conf_user = argv[1];
-    InitTemp();
+	conf_user = argv[1];
+	InitTemp();
 
-	signal(SIGTERM,signal_handler); /* catch kill signal */
-	signal(SIGINT,signal_handler); /* catch kill signal */
+	signal(SIGTERM, signal_handler);	/* catch kill signal */
+	signal(SIGINT, signal_handler);	/* catch kill signal */
 	//close(STDIN_FILENO);
 	//close(STDOUT_FILENO);
 	//close(STDERR_FILENO);
@@ -195,7 +200,7 @@ int main(int argc, char **argv)
 		if ((chdir("/tmp/")) < 0) {
 			syslog(LOG_ERR, "Could not change working directory to /tmp/\n");
 			exit(EXIT_FAILURE);
-	        }
+		}
 		syslog(LOG_NOTICE, "...now a full-featured child :)");
 		pthread_t sender_child, server_child;
 		pthread_create(&sender_child, NULL, Sender, NULL);
